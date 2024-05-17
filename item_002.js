fd5e1a5baccc0792878a1ac8d4a6fec1988c65b3
Vincent Tan <qwer012268331290@gmail.com>
2024-05-17 18:22:43 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .hintrc
    	new file:   img/BN.png
    	new file:   img/arrow.png
    	new file:   img/chat-round_svgrepo.com.png
    	new file:   img/demo_img.png
    	new file:   img/eye_svgrepo.com.png
    	new file:   img/global-refresh_svgrepo.com.png
    	new file:   img/global-refresh_svgrepo.com.svg
    	new file:   img/handbag_svgrepo.com.png
    	new file:   img/img-banner-top@3x 1.png
    	new file:   img/img.png
    	new file:   img/logo_web.png
    	new file:   img/menu_svgrepo.com.svg
    	new file:   img/profile.jpg
    	new file:   img/right-arrow_svgrepo.com.svg
    	new file:   item_002.js
    	new file:   my-app/.gitignore
    	new file:   my-app/README.md
    	new file:   my-app/package-lock.json
    	new file:   my-app/package.json
    	new file:   my-app/public/favicon.ico
    	new file:   my-app/public/index.html
    	new file:   my-app/public/logo192.png
    	new file:   my-app/public/logo512.png
    	new file:   my-app/public/manifest.json
    	new file:   my-app/public/robots.txt
    	new file:   my-app/src/App.css
    	new file:   my-app/src/App.js
    	new file:   my-app/src/App.test.js
    	new file:   my-app/src/index.css
    	new file:   my-app/src/index.js
    	new file:   my-app/src/logo.svg
    	new file:   my-app/src/reportWebVitals.js
    	new file:   my-app/src/setupTests.js
    	new file:   portfolio.css
    	new file:   portfolio.html
    
     Changes not staged for commit:
    	deleted:    portfolio.html
    
     Untracked files:
    	index.html

---
+++ item_002.js
@@ -0,0 +1,104 @@
+var skill_list = [
+{
+    type : "Figma",
+    result: "B級",
+    skill:["基礎功能：","Auto-layout：","Prototype：","Component："],
+    rank:[3,4,1,1],
+    img:["img/profile.jpg","img/profile.jpg","img/profile.jpg","img/profile.jpg"]
+},
+{
+    type : "Ai",
+    result: "C級",
+    skill:["基礎繪圖：","色彩運用：","佈局安排：","層級結構："],
+    rank:[4,3,3,4],
+    img:["img/profile.jpg","img/profile.jpg","img/profile.jpg","img/profile.jpg"]
+}
+]
+
+function switch_catagory(evt, type) {
+    var i,catagary_button,catagary_list;
+
+    catagary_button=document.getElementsByClassName("item_002_catagory_button");
+    for(i = 0 ; i<catagary_button.length ; i++){
+        catagary_button[i].classList.remove("active");
+    }
+
+    evt.currentTarget.classList.add("active");
+    //以上是控制按鈕，以下是控制內容
+
+    catagary_list=document.getElementsByClassName("item_002_list");
+    for(i=0 ; i<catagary_list.length ; i++){
+        catagary_list[i].classList.remove("call");
+    }
+
+    document.getElementById(type).classList.add("call");
+
+    //以下是重置點開的技能
+    
+    skill_active=document.getElementsByClassName("skill_active")
+    for(i = 0 ; i<skill_active.length ; i++){
+        skill_active[i].classList.remove("skill_active");
+    }
+    skill_desc=document.getElementsByClassName("item_002_skill_desc");
+    for(i = 0 ; i<skill_desc.length ; i++){
+        skill_desc[i].classList.remove("item_002_skill_desc");
+    }
+
+}
+
+function switch_skill(evt,skill){
+    var i,skill_active,skill_desc,skill_check;
+
+    skill_active=document.getElementsByClassName("skill_active")
+    for(i = 0 ; i<skill_active.length ; i++){
+        skill_active[i].classList.remove("skill_active");
+    }
+
+    skill_desc=document.getElementsByClassName("item_002_skill_desc");
+    for(i = 0 ; i<skill_desc.length ; i++){
+        skill_desc[i].classList.remove("item_002_skill_desc");
+    }
+
+    skill_check=document.getElementsByClassName(evt.currentTarget);
+    evt.currentTarget.classList.add("skill_active");
+    document.getElementById(skill).classList.add("item_002_skill_desc");
+
+    // 上面是按鈕控制,下面是資料控制 
+
+    var testing = skill_list.filter(function(x){
+        return x.type === skill
+    }); //可以用回傳變數來呼叫他
+
+    console.log(testing);
+    console.log(testing[0].result);
+
+    skill_result=document.getElementById("result");
+    skill_result.innerHTML = testing[0].result;
+
+    skill_rank=document.getElementById("test");
+    skill_rank.innerHTML = "";
+
+    for(i=0 ; i < testing[0].skill.length ; i++){
+        var liElement = document.createElement("li");
+        liElement.textContent = testing[0].skill[i];
+
+        for(x=0 ; x < testing[0].rank[i] ; x++){
+            var spanElement = document.createElement("span");
+            spanElement.classList.add("item_002_skill_point");
+            liElement.appendChild(spanElement);
+        }
+        
+        skill_rank.appendChild(liElement);
+    };
+
+    skill_image=document.getElementById("skill_image");
+    skill_image.innerHTML = "";
+    
+    for(i=0 ; i < testing[0].img.length ; i++){
+        var imgElement = document.createElement("img");
+        imgElement.src = testing[0].img[i];
+        console.log(imgElement)
+        skill_image.appendChild(imgElement);
+    }
+
+}
